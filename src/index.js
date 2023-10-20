18fa783533e48ecaa9bf70ba2a634a0b8dafa630
btsalwa <brian.tsalwa@student.moringaschool.com>
2023-10-20 18:04:07 +0300

    Tried Project

--- src/index.js
+++ src/index.js
@@ -1,23 +1,119 @@
-// Code here
-const baseApi = "http://localhost:3000" 
+const beerName = document.getElementById('beer-name');
+const beerImage = document.getElementById('beer-image');
+const beerDescription = document.getElementById('beer-description');
+const beerReviewForm = document.getElementById('review-form');
+const beerReviewText = document.getElementById('review');
+
+function beerDisplay(beer) {
+
+    const beerDescriptionForm = document.getElementById('description-form');
+    const beerEditDescription = document.getElementById('description');
+ 
+    const beerReviewList = document.getElementById('review-list');
+    while (beerReviewList.firstElementChild) {
+        beerReviewList.removeChild(beerReviewList.lastElementChild)
+    };
+
+    beerName.textContent = beer.name,
+        beerImage.src = beer.image_url,
+        beerDescription.textContent = beer.description,
+        beerEditDescription.value = beer.description
+
+     
+
+    for (let review of beer.reviews) {
+        let beerReview = document.createElement('li');
+        beerReview.textContent = review;
+        beerReviewList.appendChild(beerReview);
+    }
+
+    
+    beerDescriptionForm.addEventListener('submit', updateDescription);
+
+    
+    function updateDescription(event) {
+        
+        event.preventDefault();
+        beer.description = beerEditDescription.value;
+        updateBeer(beer)
+    };
+    
+    beerReviewForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        if (beerReviewText.value !== '') {
+
+            beer.reviews.push(beerReviewText.value)
+            updateBeer(beer)
+        } else {
+            alert('Please add Review!')
+        }
+    });
+};
 
 
-function getAndLoadAllBeers(){
-    fetch(`${baseApi }/beers`) 
 
-    .then(resp=>resp.json()) 
+function updateBeer(beer) {
+    
+    fetch(`http://localhost:3150/beers/${beer.id}`, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(beer)
+        })
+        .then(response => response.json())
+        .then(data => beerDisplay(data))
 
-    .then(beers=>{ 
+};
 
-    document.getElementById('beer-list').innerHTML = beers 
-
-    .map(beer=>`<li onClick="getAndLoadBeerDetails(${beer.id})">${beer.name}</li>`) 
-
-    .join(''); 
+function fetchData(beer = null) {
+    let baseURL = 'http://localhost:3510/beers/'
+        
+    return new Promise((resolve, ) => {
+        let url = beer == null ? baseURL : `${baseURL + beer}`
+        fetch(url)
+            .then(response => response.json())
+            .then(data => resolve(data))
 
     })
+};
 
 
-}
+
+function navDisplay(beers) {
+
     
+    const navBeerList = document.querySelector('#beer-list');
+    while (navBeerList.firstElementChild) {
+        
+        navBeerList.removeChild(navBeerList.lastElementChild)
+    };
+
+    beers.forEach(beer => {
+        const navElement = document.createElement('li');
+        navElement.textContent = beer.name;
+        navElement.setAttribute('index', beer.id);
+        navBeerList.append(navElement)
+
+        
+        navElement.addEventListener('click', (event) => {
+            
+            fetchData(event.target.getAttribute('index'))
+                .then(beer => {
+                    beerDisplay(beer);
+                });
+        });
+    });
+
+
+};
+
+function initializeFlataBeer() {
     
+    fetchData()
+        .then(beers => navDisplay(beers))
+        
+    fetchData(1)
+        .then(beers => beerDisplay(beers))
+
+};
+initializeFlataBeer()
