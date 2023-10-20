454d701b5361e8c6245a821ce0f33b0b1b00ae7a
btsalwa <brian.tsalwa@student.moringaschool.com>
2023-10-20 11:41:05 +0300

    Tried Project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 // Code here
+const baseApi = "http://localhost:3000" 
+
+
+function getAndLoadAllBeers(){
+    fetch(`${baseApi }/beers`) 
+
+    .then(resp=>resp.json()) 
+
+    .then(beers=>{ 
+
+    document.getElementById('beer-list').innerHTML = beers 
+
+    .map(beer=>`<li onClick="getAndLoadBeerDetails(${beer.id})">${beer.name}</li>`) 
+
+    .join(''); 
+
+    })
+
+
+}
+    
+    
